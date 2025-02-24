8ddaa26b269231341f8e2e3e67b083b1e8075437
immahesh555 <themahiece@gmail.com>
2025-02-24 12:04:03 +0530

    new folder

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Mahesh Kumar',
+    author_email= 'themahiece@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
